6929110f222a9e540cb6aa72ecdf503957981feb
medeadolor <dolor.medea@gmail.com>
2023-04-25 12:42:34 +0800

    Swing, JLabel, JPanel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import javax.swing.*;
+import javax.swing.JLabel;
+import  javax.swing.JPanel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        frame.setSize(300,200);
+        frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("User");
+        label.setBounds(10, 20, 80, 25);
+        panel.add(label);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 20,165, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+
+        frame.setVisible(true);
+
     }
 }
